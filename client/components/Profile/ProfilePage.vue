63ba116aa547b0fd91b713b628d634596e94f1bb
Kelsey Griffin <kelseygriffin@Kelseys-MBP-2.crimson.local>
2022-11-30 06:52:14 -0500

    style 22

--- client/components/Profile/ProfilePage.vue
+++ client/components/Profile/ProfilePage.vue
@@ -5,10 +5,10 @@
   <main>
     <section>
       <header>
-        <h2>Profile for {{ $store.state.name }}</h2>
-        <h3>Username: {{ $store.state.username }}</h3>
-        <h3 v-if="$store.state.birthday">Birthday: {{ $store.state.birthday }}</h3>
-        <h3 v-if="$store.state.bio">Bio: {{ $store.state.bio }}</h3>
+        <h2 class="profilename">Profile for {{ $store.state.name }}</h2>
+        <h3 class="profileusername"><mark>Username: {{ $store.state.username }}</mark></h3>
+        <h3 class="profilebday" v-if="$store.state.birthday">Birthday: {{ $store.state.birthday }}</h3>
+        <h3 class="profilebio" v-if="$store.state.bio">Bio: {{ $store.state.bio }}</h3>
       </header>
     </section>
     <section>
@@ -27,3 +27,37 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+* {
+  background-color: #CAE5FF;
+}
+
+.profilename {
+  color:#1B2845;
+  text-align: center;
+  font-size: 40px;
+}
+
+.profileusername {
+  color: white;
+  text-align: center;
+  font-size: 20px;
+}
+
+mark{
+  background-color: #B57BA6;
+}
+
+.profilebday {
+  color:#274060;
+  text-align: center;
+  font-size: 30px;
+}
+
+.profilebio {
+  color:#274060;
+  text-align: center;
+  font-size: 30px;
+}
+</style>
