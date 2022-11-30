4d54fd93f3a4b5363b42a81e9d5225eaa8ec1a41
Kelsey Griffin <kelseygriffin@Kelseys-MBP-2.crimson.local>
2022-11-29 20:52:46 -0500

    profileattempt7

--- client/components/Profile/ProfilePage.vue
+++ client/components/Profile/ProfilePage.vue
@@ -1,38 +1,26 @@
-<!-- Page for account settings and management -->
+<!-- Page for profile info -->
 <!-- User should be authenticated in order to see this page -->
 
 <template>
   <main>
     <section>
       <header>
-        <h2>Account settings for @{{ $store.state.username }}</h2>
+        <h2>Profile for {{ $store.state.username }}</h2>
       </header>
-      <ChangeUsernameForm />
-      <ChangePasswordForm />
     </section>
     <section>
-      <header>
-        <h2>Account management</h2>
-      </header>
-      <LogoutForm />
-      <DeleteAccountForm />
+      <ProfileComponent />
     </section>
   </main>
 </template>
 
 <script>
-import ChangeUsernameForm from '@/components/Account/ChangeUsernameForm.vue';
-import ChangePasswordForm from '@/components/Account/ChangePasswordForm.vue';
-import DeleteAccountForm from '@/components/Account/DeleteAccountForm.vue';
-import LogoutForm from '@/components/Account/LogoutForm.vue';
+import ProfileComponent from '@/components/Account/ProfileComponent.vue';
 
 export default {
-  name: 'AccountPage',
+  name: 'ProfilePage',
   components: {
-    ChangeUsernameForm,
-    ChangePasswordForm,
-    DeleteAccountForm,
-    LogoutForm
+    ProfileComponent,
   }
 };
 </script>
